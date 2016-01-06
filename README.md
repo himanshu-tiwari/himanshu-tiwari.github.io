66a77d84d8bd9daa47fc0eb9bd0442c74841889c
himanshu tiwari <htiwarih@gmail.com>
2016-01-07 04:34:22 +0530

    new files added

--- README.md
+++
@@ -1,2 +0,0 @@
-# authentication-system
-The basic authentication system required for the back end of any website using slim framework.
--- app/views/email/auth/registered.php
+++
@@ -1,6 +0,0 @@
-{% extends 'email/templates/default.php' %}
-
-{% block content %}
-	<p>You have been registered!</p>
-	<p>Click on the following link in order to activate your account: {{ baseUrl }}{{ urlFor('activate') }}?email={{ user.email }}&identifier={{ identifier|url_encode }}</p>
-{% endblock %}
