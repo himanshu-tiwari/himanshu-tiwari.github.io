5121f0da20584fe6c615dba45b9af122e5c9dc5d
himanshu tiwari <htiwarih@gmail.com>
2016-07-07 01:29:46 +0530

    updated custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,17 +1,22 @@
 $(function(){
-	$(".dropdown-menu > li > a.trigger").on("click",function(e){
-		var current=$(this).next();
-		var grandparent=$(this).parent().parent();
-		if($(this).hasClass('left-caret')||$(this).hasClass('right-caret'))
-			$(this).toggleClass('right-caret left-caret');
-		grandparent.find('.left-caret').not(this).toggleClass('right-caret left-caret');
-		grandparent.find(".sub-menu:visible").not(current).hide();
-		current.toggle();
-		e.stopPropagation();
-	});
-	$(".dropdown-menu > li > a:not(.trigger)").on("click",function(){
-		var root=$(this).closest('.dropdown');
-		root.find('.left-caret').toggleClass('right-caret left-caret');
-		root.find('.sub-menu:visible').hide();
-	});
+    $('.service-nav').on('click', function(){
+        var panelId = $(this).attr('data-panelId');
+        $(panelId).removeClass('hidden');
+    });
+});
+
+$(function(){
+    $('.prod-footer').on('click', function(){
+        $("#products").removeClass('hidden');
+        $(".hide-service").addClass('hidden');
+        $(".show-service").removeClass('hidden');
+    });
+});
+
+$(window).scroll(function() {
+  if ($(document).scrollTop() > 50) {
+    $('nav').addClass('shrink');
+  } else {
+    $('nav').removeClass('shrink');
+  }
 });
